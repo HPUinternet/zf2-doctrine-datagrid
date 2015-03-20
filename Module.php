70965eed24e8a40a23906ad9bd0fee2dc3004028
Nico Stapelbroek <nstapelbroek@gmail.com>
2015-03-20 16:54:26 +0100

    initial commit

---
+++ Module.php
@@ -0,0 +1,19 @@
+<?php namespace Wms\Admin\DataGrid;
+
+class Module {
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/',
+                ),
+            ),
+        );
+    }
+
+    public function getConfig()
+    {
+        return array_merge(include __DIR__ . '/config/module.config.php', include __DIR__ . '/config/assetmanager.config.php');
+    }
+}
---
+++ config/module.config.php
@@ -0,0 +1,9 @@
+<?php
+namespace Wms\Admin\DataGrid;
+return array(
+    'service_manager' => array(
+        'invokables' => array(
+//            'layoutBuilderService' => 'Wms\Admin\LayoutBuilder\Service\LayoutBuilderService'
+        )
+    ),
+);
