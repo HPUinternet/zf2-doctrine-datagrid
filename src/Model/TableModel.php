1a19b46c34b289a6e6051e8e42b83f029bd64e6d
Nico Stapelbroek <nstapelbroek@gmail.com>
2015-03-27 08:41:04 +0100

    Committing WIP before switching workspace

---
+++ src/Model/TableModel.php
@@ -0,0 +1,152 @@
+<?php namespace Wms\Admin\DataGrid\Model;
+
+use Zend\Stdlib\ArrayUtils;
+
+class TableModel
+{
+    /**
+     * @var array
+     */
+    protected $rows;
+
+    /**
+     * @var string
+     */
+    protected $caption;
+
+    /**
+     * @var array
+     */
+    protected $headerRow;
+
+    /**
+     * @var arrray
+     */
+    protected $attributes;
+
+    /**
+     * @param array $rows - Array of rows for the table
+     */
+    public function __construct(array $rows = array())
+    {
+        $this->rows = $rows;
+    }
+
+    /**
+     * @param string $caption Text for the HTML caption tag.
+     * @return Table
+     */
+    public function setCaption($caption)
+    {
+        $this->caption = (string)$caption;
+
+        return $this;
+    }
+
+    /**
+     * @return string The HTML caption tag text.
+     */
+    public function getCaption()
+    {
+        return $this->caption;
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasCaption()
+    {
+        return isset($this->caption);
+    }
+
+    /**
+     * @param array $rows
+     * @return Table
+     */
+    public function setRows(array $rows)
+    {
+        $this->rows = $rows;
+
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getRows()
+    {
+        return $this->rows;
+    }
+
+    /**
+     * @param array $headerRow
+     * @return Table
+     */
+    public function setHeaderRow(array $headerRow)
+    {
+        $this->headerRow = $headerRow;
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getHeaderRow()
+    {
+        // If no header row is provided, get headers from 1st rows row.
+        if (!isset($this->headerRow)) {
+            if (!count($this->rows)) {
+                return array();
+            }
+
+            return array_combine(array_keys($this->rows[0]), array_keys($this->rows[0]));
+        }
+
+        return $this->headerRow;
+    }
+
+    /**
+     * Determines if table will has a header row.
+     *
+     * A header row can be set explicitly using setHeaderRow() or by passing an
+     * associative array for the table rows.
+     *
+     * @return bool
+     */
+    public function hasHeaderRow()
+    {
+        // If no header row is provided, see if we can get headers from row keys.
+        if (!isset($this->headerRow)) {
+            return count($this->rows) ? ArrayUtils::hasStringKeys($this->rows[0]) : false;
+        }
+
+        return true;
+    }
+
+    /**
+     * @param array $attributes An associative array of tag attributes. Each key-value pair is an attribute name and value.
+     * @return Table
+     */
+    public function setAttributes(array $attributes)
+    {
+        $this->attributes = $attributes;
+
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getAttributes()
+    {
+        return $this->attributes;
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasAttributes()
+    {
+        return isset($this->attributes);
+    }
+}
--- src/Service/TableBuilderService.php
+++ src/Service/TableBuilderService.php
@@ -1,8 +1,12 @@
 <?php namespace Wms\Admin\DataGrid\Service;
 
 use Doctrine\ORM\Mapping\ClassMetadata;
-use Wms\Admin\DataGrid\Options\ModuleOptions;
 use Doctrine\ORM\EntityManager;
+use DoctrineORMModule\Paginator\Adapter\DoctrinePaginator;
+use Doctrine\ORM\Tools\Pagination\Paginator as ORMPaginator;
+use Wms\Admin\DataGrid\Options\ModuleOptions;
+use Wms\Admin\DataGrid\Model\TableModel as Table;
+use Zend\Paginator\Paginator;
 
 class TableBuilderService
 {
@@ -17,16 +21,6 @@ class TableBuilderService
      */
     protected $entityManager;
 
-    /**
-     * @var Array
-     */
-    private $hiddenColumns;
-
-    /**
-     * @var Array
-     */
-    private $columns;
-
     public function __construct(ModuleOptions $moduleOptions, EntityManager $entityManager)
     {
         $this->setModuleOptions($moduleOptions);
@@ -35,21 +29,24 @@ class TableBuilderService
 
     public function getTable()
     {
-        $this->setColumns($this->getColumnsFromEntity());
+        $table = new Table();
+        $table->setHeaderRow($this->getTableHeaders());
+        $table->setRows($this->getTableData());
     }
 
-    public function getColumnsFromEntity()
+    public function getTableHeaders()
     {
         $entityClass = $this->getModuleOptions()->getEntityName();
         if (!$entityClass) {
-            throw \Exception("No Entity found for the dataGrid module");
+            throw new \Exception("No Entity found for the dataGrid module");
         }
 
         $metaData = $this->getEntityManager()->getClassMetadata($entityClass);
         return $this->parseMetaDataToFieldArray($metaData);
     }
 
-    protected function parseMetaDataToFieldArray(ClassMetadata $metaData){
+    protected function parseMetaDataToFieldArray(ClassMetadata $metaData)
+    {
         $columns = array();
         foreach ($metaData->reflFields as $fieldName => $reflectionData) {
             if (array_key_exists($fieldName, $metaData->fieldMappings)) {
@@ -63,7 +60,49 @@ class TableBuilderService
                 throw new \Exception(sprintf('Can\'t map %s in the %s Entity', $fieldName, $metaData->name));
             }
         }
+
         return $columns;
+    }
+
+    public function getTableData()
+    {
+        $entityClass = $this->getModuleOptions()->getEntityName();
+        if (!$entityClass) {
+            throw new \Exception("No Entity found for the dataGrid module");
+        }
+
+        $repository = $this->getEntityManager()->getRepository($entityClass);
+        $qb = $repository->createQueryBuilder('test');
+
+
+
+
+
+        echo '<pre>';
+        var_dump($qb);
+        echo '</pre>';
+        die('asdfasdf');
+
+        $reflectionClass = new \ReflectionClass($entityClass);
+        $queryBuilder = $this->getEntityManager()->createQueryBuilder();
+        $queryBuilder->select("*");
+        $queryBuilder->from($entityClass, $reflectionClass->getShortName());
+        $results = $queryBuilder->getQuery()->execute();
+
+        echo '<pre>';
+        print_r($results);
+        echo '</pre>';
+        die('asdfasdf');
+
+
+        // Create the paginator itself
+        $paginator = new Paginator(
+            new DoctrinePaginator(new ORMPaginator($query))
+        );
+
+        $paginator
+            ->setCurrentPageNumber(1)
+            ->setItemCountPerPage(5);
     }
 
     /**
@@ -80,38 +119,6 @@ class TableBuilderService
     public function setModuleOptions($moduleOptions)
     {
         $this->moduleOptions = $moduleOptions;
-    }
-
-    /**
-     * @return Array
-     */
-    public function getHiddenColumns()
-    {
-        return $this->hiddenColumns;
-    }
-
-    /**
-     * @param Array $hiddenColumns
-     */
-    public function setHiddenColumns($hiddenColumns)
-    {
-        $this->hiddenColumns = $hiddenColumns;
-    }
-
-    /**
-     * @return Array
-     */
-    public function getColumns()
-    {
-        return $this->columns;
-    }
-
-    /**
-     * @param Array $columns
-     */
-    public function setColumns($columns)
-    {
-        $this->columns = $columns;
     }
 
     /**
