bb73ec62e400337c06b9ff62b7491cfca9faee76
Nico Stapelbroek <nstapelbroek@gmail.com>
2015-05-06 10:45:53 +0200

    Clean up controller, no need for getters and setters

--- src/Controller/DataGridController.php
+++ src/Controller/DataGridController.php
@@ -4,40 +4,29 @@ use Zend\Mvc\Controller\AbstractActionController;
 use Wms\Admin\DataGrid\Service\TableBuilderService;
 use Zend\View\Model\ViewModel;
 
-class DataGridController extends AbstractActionController {
+class DataGridController extends AbstractActionController
+{
 
     /**
      * @var TableBuilderService
      */
     protected $tableBuilderService;
 
-    public function __construct(TableBuilderService $tableBuilderService) {
-        $this->setTableBuilderService($tableBuilderService);
+    public function __construct(TableBuilderService $tableBuilderService)
+    {
+        $this->tableBuilderService = $tableBuilderService;
     }
 
-    public function indexAction() {
+    public function indexAction()
+    {
         if($this->params()->fromQuery('columns')) {
-            $this->getTableBuilderService()->selectColumns($this->params()->fromQuery('columns'));
+            $this->tableBuilderService->selectColumns($this->params()->fromQuery('columns'));
         }
 
         if($this->params()->fromQuery('page')) {
-            $this->getTableBuilderService()->setPage($this->params()->fromQuery('page'));
+            $this->tableBuilderService->setPage($this->params()->fromQuery('page'));
         }
 
-        return new ViewModel(array('tableData' => $this->getTableBuilderService()->getTable()));
-    }
-
-    /**
-     * @return TableBuilderService
-     */
-    public function getTableBuilderService() {
-        return $this->tableBuilderService;
-    }
-
-    /**
-     * @param TableBuilderService $tableBuilderService
-     */
-    public function setTableBuilderService($tableBuilderService) {
-        $this->tableBuilderService = $tableBuilderService;
+        return new ViewModel(array('tableData' => $this->tableBuilderService->getTable()));
     }
 }
